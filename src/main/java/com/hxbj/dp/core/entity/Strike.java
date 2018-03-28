53c2b55089b65faa9ee00695a383e614ead3f995
wanghongen <whenmm@gmail.com>
2018-03-28 13:23:17 +0800

    'builder'

--- src/main/java/com/hxbj/dp/core/entity/Strike.java
+++ src/main/java/com/hxbj/dp/core/entity/Strike.java
@@ -2,6 +2,7 @@ package com.hxbj.dp.core.entity;
 
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -13,6 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Document(collection = "t_strike")
 public class Strike implements Serializable {
 
